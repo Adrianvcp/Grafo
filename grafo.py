847e26ad64b857c4707070d03e9bf3cdc3d3d8e8
Adrianvc <adrianvc@adrianpc.adrian.vela>
2018-09-26 14:24:30 -0300

    s

--- grafo.py
+++ grafo.py
@@ -3,18 +3,94 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-import pygraphviz as pgv
+
 import pandas as pd
 
 #Diccionario de nodos y arcos
 
-def leerInformacion(Grafo):
+
+
+def leerInformacion():
     file = 'CPS.xlsx'
     x = pd.ExcelFile(file)
-    line1 = pd.read_excel(file)
-    nombre1 = line1.columns[1]
-    agregarDatosGrafo(nombre1,line1,Grafo)
+    line1 = pd.read_excel(file) #DataFrame
+    #print(line1)
+    #Grafo.add_node(nombre)7
+
+    #DataFrame por codPueblo
+
+    #DataFrame a diccionario
+    dicc_nodos = line1.set_index('CODCP').T.to_dict('list')
+
+    line1.set_index(['CODCP'], inplace=True)
+    #print(dicc_nodos)
+    GenerarGrafoDiccionario(dicc_nodos,line1)
+
+def GenerarGrafoDiccionario(dicc_nodos,dataXCodigo):
+    #print(dataXCodigo)
+    #print('--------')
+    #print(dataXCodigo.loc[683896].Y_X_COORD)
+    grafo = {}
+    a = 1
     
+    for x in dicc_nodos:
+        if len(grafo.keys()) == 0: 
+            grafo[x] = []
+             #Nodo con codigo de la ciudad
+        elif len(grafo.keys()) == 1: 
+            #coordenadas
+            #print(grafo)
+            grafo[grafo.keys()[0]] = [(x,'DISTANCIA')]
+            y,x = extraerCoordenadas(dicc_nodos[grafo.keys()[0]][1])
+            
+            
+            #y2,x2 = extraerCoordenadas(grafo[grafo.keys()[0]][1][0])
+            #print(y + " : " + x)
+            #print(y2 + " : " + x2)
+
+            #print(distance((x,y),(x2,y2)))
+            
+            grafo[x] = []
+            print(grafo.keys())
+            grafo[grafo.keys()[1]] = [(grafo.keys()[0],'DISTANCIA')]
+        
+        a+=1;
+        print(grafo)
+
+def GenerarGrafoDicci(dicc_nodos,dataXCodigo):
+    #print(dataXCodigo)
+    #print('--------')
+    #print(dataXCodigo.loc[683896].Y_X_COORD)
+    grafo = []
+
+    for x in dicc_nodos:
+        if len(grafo) == 0: 
+            grafo[x] = []
+             #Nodo con codigo de la ciudad
+        elif len(grafo) == 1: 
+            #coordenadas
+            #print(grafo)
+            grafo[grafo.keys()[0]] = [(x,'DISTANCIA')]
+            y,x = extraerCoordenadas(dicc_nodos[grafo.keys()[0]][1])
+            
+            
+            #y2,x2 = extraerCoordenadas(grafo[grafo.keys()[0]][1][0])
+            #print(y + " : " + x)
+            #print(y2 + " : " + x2)
+
+            #print(distance((x,y),(x2,y2)))
+            
+            grafo[x] = []
+            print(grafo.keys())
+            grafo[grafo.keys()[1]] = [(grafo.keys()[0],'DISTANCIA')]
+        
+        a+=1;
+        print(grafo)
+
+
+def distance(point1, point2):
+    return ((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2) ** 0.5
+
 
 def agregarDatosGrafo(nombre,dataFrame,Grafo):
     print(dataFrame.columns[1])
@@ -35,20 +111,11 @@ def extraerCoordenadas(dato):
     xy = dato.split('-')
     y = xy[1]
     x = xy[2]
-    print( str(x) + " : " + str(y))
+    
+    return y,x
 
 
 
-
-G = pgv.AGraph()
-
-leerInformacion(G)
-
-
-
-G.layout()
-
-G.draw('file.png')
-
+leerInformacion()
 
 #extraerCoordenadas('-13.992029757-72.5312662119999')
